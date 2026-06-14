7488ed1cc1821ccbc96898ef2571a45c31c81c0d
lozoviy_artur <lozoviy.artur@gmail.com>
2026-06-14 14:38:46 +0300

    added mark parametryzed tests to check list of users

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,23 @@
 from playwright.sync_api import Page, expect
+import pytest
 from pages.login_page import LoginPage
 
+@pytest.mark.parametrize("username, password", [
+    ("standard_user", "secret_sauce"),      
+    ("locked_out_user", "secret_sauce"),
+    ("problem_user", "secret_sauce"),
+    ("performance_glitch_user", "secret_sauce"),
+])
+def test_login_with_various_users(page, username, password):
+    login_page = LoginPage(page)
+    login_page.open()
+    login_page.login(username, password)
 
+    if username == "locked_out_user":
+        expect(login_page.error_message).to_be_visible()
+    else:
+        expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
+        
 def test_login_successful(page):
     login_page = LoginPage(page)
     login_page.open()
